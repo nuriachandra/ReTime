d63e0c94272799a87a64206540eedf302ba40380
AndPotap <apotapczynski@gmail.com>
2025-04-29 12:40:25 -0400

    Added gp data generation

--- data/generate_gp_data.py
+++ data/generate_gp_data.py
@@ -1,9 +1,11 @@
+from pathlib import Path
+
 import numpy as np
 
 
 def generate_rbf_data(bsz, n_points):
-    x = np.random.uniform(low=0, high=10, size=n_points)
-    ls = np.random.uniform(low=0.1, high=10, size=(bsz, 1, 1))
+    x = np.random.uniform(low=0, high=100, size=n_points)
+    ls = np.random.uniform(low=0.05, high=100, size=(bsz, 1, 1))
     sigma = np.random.uniform(low=0.1, high=1, size=(bsz, 1, 1))
     ker = rbf_kernel(x, x, params=(ls, sigma))
     return ker
@@ -18,8 +20,12 @@ def rbf_kernel(x1, x2, params):
 
 
 if __name__ == "__main__":
-    bsz, n_points = 7, 13
-    output = "./synthetic_data/ker.npy"
+    bsz, n_points = 50, 100
+    output = Path("./synthetic_data/")
+    output.mkdir(parents=True, exist_ok=True)
     ker = generate_rbf_data(bsz, n_points)
-    np.save(output, ker.astype(np.float32))
+    L = np.linalg.cholesky(ker)
+    z = np.random.uniform(low=0, high=100, size=n_points)
+    y = L @ z
+    np.save(output / "gp.npy", y.astype(np.float32))
     print(f"Data saved to {output}")
--- data/plot_data.py
+++ data/plot_data.py
@@ -3,10 +3,10 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 
 # data = np.load("./synthetic_data/test.npy")
-data = np.load("./synthetic_data/ker.npy")
+data = np.load("./synthetic_data/gp.npy")
 
-# caseS = [10, 20, 30, 40, 50]
-caseS = [1, 3, 5]
+caseS = [10, 20, 30, 40]
+# caseS = [1, 3, 5]
 # caseS = [i for i in range(100)]
 
 sns.set(style="whitegrid", font_scale=4)
@@ -16,6 +16,7 @@ xgrid = np.arange(data.shape[-1])
 for cdx in caseS:
     fig, axs = plt.subplots(1, 1, figsize=(10, 12), dpi=100)
     axs, idx = [axs], 0
-    axs[idx].scatter(xgrid, data[cdx])
-    axs[idx].plot(xgrid, data[cdx])
+    axs[idx].scatter(xgrid, data[cdx], lw=4)
+    axs[idx].plot(xgrid, data[cdx], lw=4)
+    plt.tight_layout()
     plt.show()
